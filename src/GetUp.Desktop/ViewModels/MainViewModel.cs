2f76351884bc1a42ef16c94461190ca9cdd9cc27
Julien Poulin <poulin_julien@hotmail.com>
2015-05-01 14:02:14 +0200

    Added MainViewModel.

---
+++ src/GetUp.Desktop/ViewModels/MainViewModel.cs
@@ -0,0 +1,28 @@
+﻿// -----------------------------------------------------------------------
+// <copyright file="MainViewModel.cs" company="WebForAll">
+//   Copyright © WebForAll. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Julien Poulin</author>
+// <date>29/04/2015</date>
+// <project>GetUp.Desktop</project>
+// <web>http://www.webforall.be</web>
+// -----------------------------------------------------------------------
+
+namespace GetUp.Desktop.ViewModels
+{
+
+  using GalaSoft.MvvmLight;
+
+  public class MainViewModel : ViewModelBase
+  {
+
+    public bool IsEnabled { get; set; }
+
+    public int Hour { get; set; }
+
+    public int Minute { get; set; }
+
+  }
+
+}
