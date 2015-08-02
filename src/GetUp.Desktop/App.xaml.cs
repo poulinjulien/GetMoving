59dea3f98cf4e4bc784b7db91c2d57c82e89d92b
Julien Poulin <poulin_julien@hotmail.com>
2015-08-02 13:16:40 +0200

    Added tray icon.

--- src/GetUp.Desktop/App.xaml.cs
+++ src/GetUp.Desktop/App.xaml.cs
@@ -1,17 +1,20 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿// -----------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="WebForAll">
+//   Copyright © WebForAll. All rights reserved.
+// </copyright>
+// -----------------------------------------------------------------------
+// <author>Julien Poulin</author>
+// <date>26/03/2015</date>
+// <project>GetUp.Desktop</project>
+// <web>http://www.webforall.be</web>
+// -----------------------------------------------------------------------
 
 namespace GetUp.Desktop
 {
-  /// <summary>
-  /// Interaction logic for App.xaml
-  /// </summary>
-  public partial class App : Application
+
+  public partial class App
   {
+
   }
+
 }
--- src/GetUp.Desktop/MainWindow.xaml.cs
+++ src/GetUp.Desktop/MainWindow.xaml.cs
@@ -12,16 +12,38 @@
 namespace GetUp.Desktop
 {
 
+  using System;
   using System.Windows;
+  using System.Windows.Input;
+  using GalaSoft.MvvmLight.Command;
   using ViewModels;
 
-  public partial class MainWindow : Window
+  public partial class MainWindow
   {
+
+    private ICommand _RestoreWindowCommand;
 
     public MainWindow()
     {
       InitializeComponent();
       DataContext = new MainViewModel();
+    }
+
+    public ICommand RestoreWindowCommand
+    {
+      get { return _RestoreWindowCommand ?? (_RestoreWindowCommand = new RelayCommand(RestoreWindow)); }
+    }
+    
+    private void RestoreWindow()
+    {
+      WindowState = WindowState.Normal;
+    }
+
+    protected override void OnStateChanged(EventArgs e)
+    {
+      base.OnStateChanged(e);
+
+      ShowInTaskbar = WindowState == WindowState.Normal;
     }
 
   }
